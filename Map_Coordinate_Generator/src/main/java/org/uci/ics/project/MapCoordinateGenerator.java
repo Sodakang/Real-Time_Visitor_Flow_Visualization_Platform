e6a46b129b79436dddb562f38c89e511a427297d
Sodakang <inlandriver@outlook.com>
2020-06-03 21:47:24 -0700

    Add the map coordinate generator.

---
+++ Map_Coordinate_Generator/src/main/java/org/uci/ics/project/MapCoordinateGenerator.java
@@ -0,0 +1,14 @@
+package org.uci.ics.project;
+
+/**
+ * Generate the virtual coordinate on map based on latitude and longitude.
+ *
+ */
+public class MapCoordinateGenerator
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Map_Coordinate_Generator/src/test/java/org/uci/ics/project/MapCoordinateGeneratorTest.java
@@ -0,0 +1,20 @@
+package org.uci.ics.project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MapCoordinateGeneratorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
